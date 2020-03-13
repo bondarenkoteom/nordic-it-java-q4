b13f3fab749f526aac5f3eac3fe61813d5417c91
Артем Бондаренко <bondarenko.teom@yandex.ru>
2020-03-13 21:10:50 +0300

    123

---
+++ 11/src/main/java/com/homework1/parking/Place.java
@@ -0,0 +1,18 @@
+package com.homework1.parking;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Place {
+
+   private Integer number; 
+   private boolean park;
+   
+   
+
+}
